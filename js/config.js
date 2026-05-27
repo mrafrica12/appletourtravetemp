7d53cd65e11ca8e81ef8f5b7e1c3f9b2386f7c5d
Ben Ngie <katheka@gmail.com>
2026-05-26 23:23:11 -0400

    First upload
    First upload

---
+++ js/config.js
@@ -0,0 +1,5 @@
+window.APPLE_SAFARIS_CONFIG = {
+  whatsAppNumber: '255742000000',
+  appsScriptEndpoint: 'PASTE_YOUR_DEPLOYED_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE',
+  publicFormToken: 'apple-safaris-public-forms',
+};
