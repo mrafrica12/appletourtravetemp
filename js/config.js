e1953489760f5456276c22220fec3713a3034eb4
Ben Ngie <katheka@gmail.com>
2026-05-28 12:38:09 -0400

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-window.APPLE_SAFARIS_CONFIG = {
-  whatsAppNumber: '255742000000',
-  appsScriptEndpoint: 'https://script.google.com/macros/s/AKfycbwLBpgHV9lZTRTXlRv9ZZwD-uu-uiTmALO_YiV05XXsd5OMzVtKvkq65wfAM-lC8Z0euw/exec',
-  publicFormToken: 'apple-safaris-public-forms',
-};
